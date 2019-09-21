8a4ff3dcf3e97c67d82ec15403cf83f7be129d6f
Mike Baynton <mike@mbaynton.com>
2019-09-21 11:41:19 -0500

    prompt rough example

--- Plugin.php
+++ Plugin.php
@@ -53,6 +53,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface, Capable {
     return [
       ScriptEvents::POST_UPDATE_CMD => 'postCmd',
       ScriptEvents::POST_INSTALL_CMD => 'postCmd',
+      ScriptEvents::POST_CREATE_PROJECT_CMD => 'postProject',
       PackageEvents::POST_PACKAGE_INSTALL => 'postPackage',
       PluginEvents::COMMAND => 'onCommand',
     ];
@@ -88,6 +89,41 @@ class Plugin implements PluginInterface, EventSubscriberInterface, Capable {
     if ($event->getCommandName() == 'require') {
       $this->handler->beforeRequire($event);
     }
+  }
+
+  public function postProject(Event $event) {
+      if (function_exists('posix_isatty') && posix_isatty(STDIN)) {
+          echo <<<GITIGNORE_PREFS_DISCUSSION
+.gitignore preference
+=====================
+If you use git to manage your new site, which files are best committed to git
+will vary depending on how you deploy changes to your production website.
+
+How you answer this quesiton will only affect which initial .gitignore file we
+get you started with; if you are unsure how to answer or don't know how changes
+will be deployed to production yet, you can always adjust your .gitignore file
+later.
+
+Some people prefer to commit all the files Composer has downloaded into their
+git repository, and use git by itself to deploy changes. Others prefer to commit
+only their project's unique modules and themes to their repository, and then
+configure a more complex deployment strategy.
+
+GITIGNORE_PREFS_DISCUSSION;
+
+          do {
+              echo <<<GITIGNORE_PREFS_PROMPT
+Would you like to:
+  1) Use a .gitignore that includes Composer-managed files in the repository
+  2) Use a .gitignore that ignores all locations Composer writes files
+  3) Write no .gitignore file now
+
+1-3? 
+GITIGNORE_PREFS_PROMPT;
+
+              $input = fgets(STDIN);
+          } while (! preg_match('/^(?:1|2|3)\s*/', $input));
+      }
   }
 
 }
